58fe53f21d6f1c6a94b467061c3d8346e95a1fd7
stla <laurent_step@yahoo.fr>
2023-11-04 00:49:43 +0100

    pentagram example

--- R/interpolators.R
+++ R/interpolators.R
@@ -61,6 +61,22 @@ iprBarycentricRational <- function(x, y, ao = 3) {
 #' head(Curve)
 #' plot(Curve, type = "l", lwd = 2)
 #' points(points, pch = 19)
+#'
+#' # a closed example (pentagram) ####
+#' rho <- sqrt((5 - sqrt(5))/10)
+#' R <- sqrt((25 - 11*sqrt(5))/10)
+#' points <- matrix(NA_real_, nrow = 10L, ncol = 2L)
+#' points[c(1, 3, 5, 7, 9), ] <- t(vapply(0:4, function(i){
+#'   c(rho*cospi(2*i/5), rho*sinpi(2*i/5))
+#' }, numeric(2L)))
+#' points[c(2, 4, 6, 8, 10), ] <- t(vapply(0:4, function(i){
+#'   c(R*cospi(2*i/5 + 1/5), R*sinpi(2*i/5 + 1/5))
+#' }, numeric(2L)))
+#' ipr <- iprCatmullRom(points, closed = TRUE)
+#' s <- seq(0, 1, length.out = 400L)
+#' Curve <- evalInterpolator(ipr, s)
+#' plot(Curve, type = "l", lwd = 2, asp = 1)
+#' points(points, pch = 19)
 iprCatmullRom <- function(points, closed = FALSE, alpha = 0.5) {
   stopifnot(is.matrix(points))
   stopifnot(ncol(points) %in% c(2L, 3L))
--- inst/essais/essai04_CatmullRom_pentagram.R
+++ inst/essais/essai04_CatmullRom_pentagram.R
@@ -1,24 +1,18 @@
 library(interpolators)
 
+# a closed example (pentagram) ####
 rho <- sqrt((5 - sqrt(5))/10)
-vs1 <- t(vapply(0:4, function(i){
-  c(rho*cos(2*i*pi/5), rho*sin(2*i*pi/5))
-}, numeric(2L)))
 R <- sqrt((25 - 11*sqrt(5))/10)
-vs2 <- t(vapply(0:4, function(i){
-  c(R*cos(2*i*pi/5 + pi/5), R*sin(2*i*pi/5 + pi/5))
-}, numeric(2L)))
 points <- matrix(NA_real_, nrow = 10L, ncol = 2L)
-points[c(1, 3, 5, 7, 9),] <- vs1
-points[c(2, 4, 6, 8, 10),] <- vs2
-
-
-
-
-ipr <- iprCatmullRom(points, closed = TRUE, alpha = 1)
-s <- seq(0, 1, length.out = 400)
+points[c(1, 3, 5, 7, 9), ] <- t(vapply(0:4, function(i){
+  c(rho*cospi(2*i/5), rho*sinpi(2*i/5))
+}, numeric(2L)))
+points[c(2, 4, 6, 8, 10), ] <- t(vapply(0:4, function(i){
+  c(R*cospi(2*i/5 + 1/5), R*sinpi(2*i/5 + 1/5))
+}, numeric(2L)))
+ipr <- iprCatmullRom(points, closed = TRUE)
+s <- seq(0, 1, length.out = 400L)
 Curve <- evalInterpolator(ipr, s)
-head(Curve)
 plot(Curve, type = "l", lwd = 2, asp = 1)
 points(points, pch = 19)
 
